f7d09f53c3f04efabf34a4367362eae519d774b5
AYABA <tobilobamurele@gmail.com>
2022-08-20 21:23:34 +0100

    dashboard

--- ultrlfront/src/components/Dashboard.js
+++ ultrlfront/src/components/Dashboard.js
@@ -3,6 +3,8 @@ import { isAuthenticated } from '../auth'
 // import { Redirect } from 'react-router-dom'
 import Posts from '../posts/Posts'
 import Nav from './Nav'
+import likeimg from '../assets/like.png'
+import commentimg from '../assets/comment.png'
 
 const Dashboard = () => {
   return (
@@ -11,11 +13,13 @@ const Dashboard = () => {
         <div>
           <Nav />
           <Posts />
+          <img src={likeimg} className="p-1 space-x-1" />
+          <img src={commentimg} className="p-1" />
         </div>
       }
       {!isAuthenticated() &&
         <>
-          deisgn this page
+         design this page
         </>
       }
     </div>
