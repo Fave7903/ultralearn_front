b545d25a2bf78a142f7916b040c92dcf1408eb1e
AYABA <tobilobamurele@gmail.com>
2022-09-05 22:18:57 +0100

    white logo

--- ultrlfront/src/components/logotext.js
+++ ultrlfront/src/components/logotext.js
@@ -5,7 +5,7 @@ export const Logotext = (small = false) => {
     return (
     
         <div className={"flex flex-row" + (( !small)?"m-6 ml-9":" m-2")}>
-            <span><img style={{ width: "60px", height: "55px" }} src={ultralearn} className="image-fluid mx-1 mt-0" alt="Student" /></span> 
-            <div className="text-3xl text-white font-semibold m-0.5	" >UltraLEARN</div>
+            <span><img style={{ width: "55px", height: "50px" }} src={ultralearn} className="image-fluid mx-1 mt-0" alt="Student" /></span> 
+            <div className="text-3xl text-white font-semibold mt-1.5	" >UltraLEARN</div>
         </div>)
 }
