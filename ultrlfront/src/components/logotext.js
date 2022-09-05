9eef88f8796abc8466c4495ff24d2ddabfb8f13f
AYABA <tobilobamurele@gmail.com>
2022-09-05 17:32:53 +0100

    white logo

--- ultrlfront/src/components/logotext.js
+++ ultrlfront/src/components/logotext.js
@@ -1,11 +1,11 @@
 import React from "react";
-import ultralearn from '../assets/ultralearn white.png'
+import ultralearn from '../assets/ultralearn white2.png'
 
 export const Logotext = (small = false) => {
     return (
     
         <div className={"flex flex-row" + (( !small)?"m-6 ml-9":" m-2")}>
-            <span><img style={{ width: "55px", height: "50px" }} src={ultralearn} className="image-fluid mx-1 mt-0" alt="Student" /></span> 
-            {/* <div className="text-3xl ul-purple-700 font-semibold	" >UltraLEARN</div> */}
+            <span><img style={{ width: "60px", height: "55px" }} src={ultralearn} className="image-fluid mx-1 mt-0" alt="Student" /></span> 
+            <div className="text-3xl text-white font-semibold m-0.5	" >UltraLEARN</div>
         </div>)
 }
