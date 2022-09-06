cb891d04bb0d5b1dbaf7af9bc6261ee1e7760546
Adeboluwarin <adeboluwarinthomas@gmail.com>
2022-09-06 01:00:08 +0100

    Logo

--- ultrlfront/src/components/logotext.js
+++ ultrlfront/src/components/logotext.js
@@ -5,7 +5,7 @@ export const Logotext = (small = false) => {
     return (
     
         <div className={"flex flex-row" + (( !small)?"m-6 ml-9":" m-2")}>
-            <span><img style={{ width: "55px", height: "50px" }} src={ultralearn} className="image-fluid mx-1 mt-0" alt="Student" /></span> 
-            <div className="text-3xl text-white font-semibold mt-1.5	" >UltraLEARN</div>
+            <span><img style={{ width: "40px", height: "40px" }} src={ultralearn} className="image-fluid mx-1 mt-0" alt="Student" /></span> 
+            <div className="text-3xl text-white font-semibold mt-1	" >UltraLEARN</div>
         </div>)
 }
