7c63fb8eb79aa2588d078ca7ce72194c64ae7098
Adeboluwarin <adeboluwarinthomas@gmail.com>
2022-09-19 02:36:44 +0100

    Add Friends

--- ultrlfront/src/components/FindPeople.js
+++ ultrlfront/src/components/FindPeople.js
@@ -62,15 +62,16 @@ class FindPeople extends Component {
               return false
             }
           }).map((user, i) => (
-            <div className="px-2 sm:px-16 flex grid grid-cols-2 sm:grid grid-cols-3 mb-8" key={i}>
-  <div className="">
+            <div className="px-2 sm:px-16 flex grid grid-cols-2 sm:grid grid-cols-2 mb-8" key={i}>
+  <div className="flex gap-7">
   {user.imgId ? <Image cloudName="favoursoar" publicId={user.imgId} style={{width: '100px', height: '100px', objectFit: 'cover', borderRadius: '50%', borderColor: "purple"}}/> :
           <img src={avatar}alt="Avatar" className=" invisble sm:visible rounded-full"style={{color: "#460273", width: "100px",height:"100px"}}/>
           }
+            <Link to={`/users/${user.username}`} className=" text-dark text-1xl sm:text-2xl" ><h5 className="">{user.fullName}</h5></Link>
   </div>
-  <Link to={`/users/${user.username}`} className=" text-dark text-1xl sm:text-2xl" ><h5 className="">{user.fullName}</h5></Link>
+
   <div className=''>
-    <button style={{backgroundColor: "#460273", color: "white"}} onClick={() => this.clickFollow(user, i)} className=' float-right py-4 px-8'>Follow</button>
+    <button style={{backgroundColor: "#460273", color: "white"}} onClick={() => this.clickFollow(user, i)} className=' float-right py-4 px-12'>Follow</button>
     </div>
 </div>
           ))}
@@ -81,10 +82,10 @@ class FindPeople extends Component {
     const {users, open, error, followMessage, loading} = this.state
     
     return (
-      <div className='container pt-5 mt-5'>
+      <div className=' '>
         <Nav />
 
-        <div className="-mb-10 sm:mb-8  " >
+        <div className="-mb-10 sm:mb-8 mt-10 " >
           <div className="input-group-prepend">
             <span className="input-group-text" id="basic-addon1"><i className="fas fa-search"></i></span>
           </div>
@@ -94,7 +95,7 @@ class FindPeople extends Component {
         </div>
         <Errormsg error={error} style={{ display: error ? "" : "none" }} />
         {open && (
-        <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
+        <div class="bg-green-100 border border-green-400 text-green-700 mx-6 my-6 px-4 py-3 rounded relative" role="alert">
         <span class="block sm:inline">{followMessage}</span>
         <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
             <svg class="fill-current h-6 w-6 text-green-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" /></svg>
