fad235128a2699497a5abbde222fe2e5047bfc36
Abiodun <emmi34.et@gmail.com>
2022-10-02 17:53:32 +0100

    update

--- ultrlfront/src/components/logotext.js
+++ ultrlfront/src/components/logotext.js
@@ -1,11 +1,11 @@
 import React from "react";
-import ultralearn from '../assets/ultralearn white2.png'
+import ultralearn from '../assets/Logo.png'
 
 export const Logotext = (small = false) => {
     return (
     
         <div className={"flex flex-row" + (( !small)?"m-6 ml-9":" m-2")}>
-            <span><img style={{ width: "45px", height: "55px" }} src={ultralearn} className="image-fluid mx-3 mt-0" alt="Student" /></span> 
-            <div style={{ color: "#FFFFFF" }}className="text-3xl font-semibold mt-2	" >UltraLEARN</div>
+            <span><img style={{ width: "65px", height: "55px" }} src={ultralearn} className="image-fluid mx-1 mt-0" alt="Student" /></span> 
+            <div style={{ color: "#460273" }}className="text-3xl font-semibold mt-2	" >UltraLEARN</div>
         </div>)
 }
